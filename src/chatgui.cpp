c5fcd086b833a6ea69bb3d9c31f23add1fb97832
hessam94 <azzurro_94@yahoo.com>
2023-01-07 21:29:16 -0500

    apply unique pointer to chatlogic

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -5,6 +5,7 @@
 #include "chatbot.h"
 #include "chatlogic.h"
 #include "chatgui.h"
+#include <memory>
 
 // size of chatbot window
 const int width = 414;
@@ -118,7 +119,9 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    //_chatLogic = new ChatLogic();
+    std::unique_ptr<ChatLogic> temp (new ChatLogic());
+    _chatLogic = std::move(temp);
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +138,7 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    //delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
